dba17b040eaf3b86e711d53f88228a8d879bc2af
Brian H <bheptin@gmail.com>
2016-08-16 20:59:27 -0400

    end push

--- homework.js
+++ homework.js
@@ -69,37 +69,41 @@ console.assert(inAnObject.anotherObject.test2('twitter', 'book') === '[object Ob
 
 console.assert(whatIsThis.call() === "[object Window],,")
 // Once you've figured out what the output is, answer here in a comment: Why is this so?
-///
+///Still calling the global output. this time, no a or b, but still the "join" function
 
 
 console.assert(whatIsThis.call(trickyTricky) === "[object Object],,")
 // Once you've figured out what the output is, answer here in a comment: Why is this so?
-
+///Same as before, but this time, used trickyTricky, but didn't use the keys to call anything
+///so, nothing returned.
 
 
 console.assert(whatIsThis.call(trickyTricky, 'nice', 'job') === "[object Object],nice,job")
 // Once you've figured out what the output is, answer here in a comment: Why is this so?
-
+///This returns the array and nice job per the (a,b) in whatIsThis.
 
 
 console.assert(whatIsThis.call(confusing) === "[object Object],,")
 // Once you've figured out what the output is, answer here in a comment: Why is this so?
-
+///Same as before, but this time, used confusing, but didn't use the keys to call anything
+///so, nothing returned
 
 
 console.assert(whatIsThis.call(confusing, 'hello') === "[object Object],hello,")
 // Once you've figured out what the output is, answer here in a comment: Why is this so?
-
+///whatIsThis is calling on something it can't use, but still returns hello, because it's
+///set up correctly.
 
 
 console.assert(whatIsThis.apply(trickyTricky) === "[object Object],,")
 // Once you've figured out what the output is, answer here in a comment: Why is this so?
-
+///Refer to the last time, this happened. Pulled back whatIsThis as an array, but nothing
+///to use for the (a,b). It has to be setup as an array.
 
 
 console.assert(whatIsThis.apply(confusing, ['nice', 'job']) === "[object Object],nice,job")
 // Once you've figured out what the output is, answer here in a comment: Why is this so?
-
+///whatIsThis is called, then apply to get an array, with the (a,b) formula.
 
 
 console.assert(whatIsThis.apply(confusing, 'nice', 'job') === FILL_ME_IN)
@@ -110,22 +114,23 @@ console.assert(whatIsThis.apply(confusing, 'nice', 'job') === FILL_ME_IN)
 
 console.assert(inAFunction('what will', 'happen?') === '[what will, happen?]')
 // Once you've figured out what the output is, answer here in a comment: Why is this so?
-
+///function still uses the same setup, because it's two arguements being combined into one.
 
 
 try{
-    console.assert(inAFunction.test3('A', 'B') === '[what will, happen?]')
+    console.assert(inAFunction.test3('A', 'B') === FILL_ME_IN)
 } catch(e){
     log(e)
 }
 // Once you've figured out what the output/result is, answer here in a comment: Why is this so?
-
+///can't be worked. Wording in the arguement uses two functions.
 
 
 var newObject = new inAFunction('what will', 'happen?')
 console.assert(newObject.name === 'Sally')
 // Once you've figured out what the output is, answer here in a comment: Why is this so?
-
+///inAFunction was never redefined. It returns it's original "this". This is still attached 
+///to it
 
 
 var newObject2 = new inAFunction('what will', 'happen?')
